b40fb8a98cded5b487df5551e2c33c1a6cf7ccfc
Vivek Mane <vivekmane172@gmail.com>
2025-10-02 21:19:56 +0530

    Worked on javascript of menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ hbutton.addEventListener('click', function () {
     
 });
 
+
+
+ let button = document.getElementById('hamburger');
+    let element = document.getElementById('toggling');
+    let menu = document.getElementById('menu');
+    button.addEventListener('click', function () {
+        // element.classList.toggle('block');
+        if(element.style.display == 'none'){
+            element.style.display = 'block';
+        }
+        else{
+            element.style.display= 'none';
+        }
+            
+    });
+
+    window.addEventListener('click',function(event)
+{
+    if(!menu.classList.contains('hidden') && !meny.contains(event.target)){
+        // meny.classList.add('hidden');
+        element.style.display= 'none';
+    }
+})
